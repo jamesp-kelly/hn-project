2950bb986d627bcd3c10e4ab91fdb9aedaa13090
James Kelly <jk_jameskelly@outlook.com>
2017-04-12 14:25:44 -0400

    snapshot tests with Jest

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -26,6 +27,7 @@ class App extends Component {
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -158,6 +160,13 @@ const Search = ({
         {children}
       </button>
     </form>
+  
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+};
 
 const Table = ({list, onDismiss }) => 
   <div className="table">
@@ -185,7 +194,19 @@ const Table = ({list, onDismiss }) =>
     )}      
   </div>
 
-const Button = ({onClick, className = '', children}) =>
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      url: PropTypes.string,
+      title: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.points
+    })).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
+
+const Button = ({onClick, className, children}) =>
   <button
     onClick={onClick}
     className={className}
@@ -194,5 +215,21 @@ const Button = ({onClick, className = '', children}) =>
     {children}
   </button>
 
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+Button.defaultProps = {
+  className: ''
+};
+
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import App, {Search, Button, Table} from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+describe('App', () => {
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Search', () => {
+
+  const props = {
+    onSubmit: () => {},
+    onChange: () => {},
+    value: ''
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search { ...props} >Search</Search>, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(<Search { ...props}>Search</Search>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+
+  const props = {
+    onClick: () => {}
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button { ...props}>More</Button>, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(<Button { ...props}>More</Button>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }
+    ],
+    onDismiss: () => {}
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props} />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Table { ...props} />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
